9e8d86bb15d3017864fdba5bec6653af5bdee462
nguyenquan-2510 <quannguyenminhbl@gmail.com>
2025-11-12 21:20:24 +0700

    imported necessary libraries and tries some code

--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
 
 function love.load()
+    -- external libs
+    sti = require 'libs/sti'
+    anim8 = require 'libs/anim8'
+    cam = require 'libs/camera'
+    wf = require 'libs/windfield'
+
+    world = wf.newWorld(0, 0)
+    rect1 = world:newRectangleCollider(100, 100, 200, 100)
 end
 
 function love.update(dt)
+    world:update(dt)
 end
 
 function love.draw()
-    love.graphics.circle("line", 200, 200, 100)
+    world:draw()
 end
