2d88e7090344df08968c79828728e59206131b02
nguyenquan-2510 <quannguyenminhbl@gmail.com>
2025-11-15 21:34:24 +0700

    remove unused code

--- Player/init.lua
+++ Player/init.lua
@@ -4,12 +4,7 @@ local player = {}
 player.idle_sheet = love.graphics.newImage("Player/idle/idle_down.png")
 player.run_sheet = love.graphics.newImage("Player/run/run_down.png")
 
--- player.spritesheet = love.graphics.newImage("Player/char_spritesheet.png")
--- player.grid = anim8.newGrid(32, 32,player.spritesheet:getWidth(), player.spritesheet:getHeight())
-
--- player.animations = {}
--- player.animations.down = anim8.newAnimation(player.grid('1-6', 1), 0.2)
-
+-- Store animate frames
 local animations = {}
 animations.__index = animations
 
--- main.lua
+++ main.lua
@@ -2,18 +2,16 @@
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 
-    require 'src/load'
+    require 'src/load' -- Load external libs, initialize the obstables' position and size
 end
 
 function love.update(dt)
-    -- player.animations.down:update(dt)
     player.idle_anim.down:update(dt)
 
     world:update(dt)
 end
 
 function love.draw()
-
     -- Draw map
     render.draw_scaled_layer(map, "general_ground", 4)
     render.draw_scaled_layer(map, "detail_ground", 4)
@@ -22,6 +20,5 @@ function love.draw()
     -- Draw colliders (walls)
     render.draw_with_scale(function() world:draw() end, nil, 4)
 
-    -- player.animations.down:draw(player.spritesheet, 50, 50)
     player.idle_anim.down:draw(player.idle_sheet, 100, 100, 0, 4, 4)
 end
