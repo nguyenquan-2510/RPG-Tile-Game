eb4dd7a0fa25640d67c2ad1c73e4a6c40390c96f
nguyenquan-2510 <quannguyenminhbl@gmail.com>
2025-11-17 14:15:32 +0700

    easier control over animation state

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 
-    require 'src.ext_load' -- Load external libs (anim8, sti, windfield, camera)
+    require 'src/ext_load' -- Load external libs (anim8, sti, windfield, camera)
 
     -- Initialize new world
     world = wf.newWorld(0, 0)
@@ -15,19 +15,38 @@ function love.load()
 
     -- Draw the collision
     walls = {}
-    require 'src.collision_init'
+    require 'src/collision_init'
     init_collision_wall(map, "collision_rect", walls)
 
     -- The player and some contracted forms of variables
     player = require 'Player'
-    require 'src.definitions' -- Some useful definitions
+    require 'src/animation_def' -- Some useful definitions
+
     player.dir = "down"
-    player.sheet_dir = "spritesheet_" .. player.dir
-    player.state = idle[player.dir]
+    player.state = "idle"
+
+    -- Name of the animation sheet
+    player.sheet_name = player.state .. "_" .. player.dir .. "_sheet"
+    player.sheet_dir = anim[player.sheet_name]
+
+    player.anim = anim[player.state][player.dir]
+end
+
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+
+    if key == "right" or key == "left" or key == "up" or key == "down" then
+        player.dir = tostring(key)
+        player.sheet_name = player.state .. "_" .. player.dir .. "_sheet"
+        player.sheet_dir = anim[player.sheet_name]
+    end
 end
 
 function love.update(dt)
-    player.state:update(dt)
+    player.anim:update(dt)
     world:update(dt)
 end
 
@@ -41,22 +60,10 @@ function love.draw()
     map:drawLayer(map.layers["detail_ground"])
     map:drawLayer(map.layers["objects"])
 
-    player.state:draw(idle[player.sheet_dir], player.x, player.y, 0, 1, 1)
+    player.anim:draw(player.sheet_dir, player.x, player.y, 0, 1, 1)
     world:draw()
 
     love.graphics.pop()
 
     cam:detach()
-end
-
-function love.keypressed(key)
-    if key == "escape" then
-        love.event.quit()
-    end
-
-    if key == "right" or key == "left" or key == "up" or key == "down" then
-        player.dir = tostring(key)
-        player.sheet_dir = "spritesheet_" .. player.dir
-        player.state = idle[player.dir]
-    end
 end
---
+++ src/animation_def.lua
@@ -0,0 +1,28 @@
+-- Shorten the code haha
+
+anim =
+{
+    ["idle"] = {
+        ["down"] = player.idle.down,
+        ["up"] = player.idle.up,
+        ["left"] = player.idle.left,
+        ["right"] = player.idle.right
+    },
+
+    ["run"] = {
+        ["down"] = player.run.down,
+        ["up"] = player.run.up,
+        ["left"] = player.run.left,
+        ["right"] = player.run.right
+    },
+
+    ["idle_down_sheet"] = player.idle_sheet.down,
+    ["idle_up_sheet"] = player.idle_sheet.up,
+    ["idle_left_sheet"] = player.idle_sheet.left,
+    ["idle_right_sheet"] = player.idle_sheet.right,
+
+    ["run_down_sheet"] = player.run_sheet.down,
+    ["run_up_sheet"] = player.run_sheet.up,
+    ["run_left_sheet"] = player.run_sheet.left,
+    ["run_right_sheet"] = player.run_sheet.right,
+}
